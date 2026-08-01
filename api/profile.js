30e322566c740c2795cf27ed593c006f95057f71
RAFIQUL ISLAM <126047983+Sanvi009@users.noreply.github.com>
2026-08-01 11:06:45 +0600

    Update profile.js

--- api/profile.js
+++ api/profile.js
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
         last_username_change,
         last_display_name_change
       `)
-      .eq('id', id)
+      .eq('username', targetUserId)
       .single();
 
     if (error || !profile) {
